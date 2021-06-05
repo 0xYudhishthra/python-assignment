7898f556186d5320069a33e815e2323f1a24c82e
Yudhish <tectrixtech2@gmail.com>
2021-06-06 02:54:47 +0800

    Update pseudocode

--- main.py
+++ main.py
@@ -2,15 +2,8 @@
 #Choong Wei Jun - TP061867
 
 #!/usr/bin/env python3
-from pyfiglet import Figlet # Importing module to decorate welcome message
 import os, time
 
-'''
-This part of code is to display the name of the program
-'''
-from pyfiglet import Figlet
-f = Figlet(font='univers')
-print (f.renderText('S O F S'))
 
 def main() :
     print("Types of dashboard:\n1. Customer\n2. Administrator\nQ. Quit")
